1989d18a24d76cd096e735fbdb81fc50116c9076
vik <1833970@alu.murciaeduca.es>
2026-05-26 12:59:13 +0200

    Panel logic

--- Assets/Scripts/EventsController.cs
+++ Assets/Scripts/EventsController.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class EventsController : MonoBehaviour
 {
+    [Header("Time")]
     public float timeRemaning;
     [SerializeField] TextMeshProUGUI txt_timer;
+    [Header("Panels")]
+    [SerializeField] GameObject panelPlay;
+    [SerializeField] GameObject panelInGame;
+    [SerializeField] GameObject panelGameOver;
+    [SerializeField] GameObject panelGameWon;
+    [Header("Other")]
+    [SerializeField] Button buttonPlay;
+    int points = 0;
     
+
     void Start()
     {
-        
+        Time.timeScale = 0;
+        panelPlay.SetActive(true);
+        panelInGame.SetActive(false);
+        panelGameOver.SetActive(false);
+        panelGameWon.SetActive(false);
     }
 
     
@@ -21,10 +36,24 @@ public class EventsController : MonoBehaviour
         else if (timeRemaning < 0)
         {
             timeRemaning = 0;
+            Time.timeScale = 0;
         }
 
         int min = Mathf.FloorToInt(timeRemaning / 60);
         int sec = Mathf.FloorToInt(timeRemaning % 60);
         txt_timer.text = string.Format("{0:00}:{1:00}",min, sec);
     }
+
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        timeRemaning = 120;
+        points = 0;
+        panelPlay.SetActive(false);
+        panelInGame.SetActive(true);
+        panelGameOver.SetActive(false);
+        panelGameWon.SetActive(false);
+    }
+    
 }
